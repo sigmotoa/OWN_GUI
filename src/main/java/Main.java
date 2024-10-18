af4fd52e29fd9715f90eec1949d422382895dd6a
sigmotoa <si.galvismotoa@gmail.com>
2024-10-18 13:10:56 -0500

    Refactor heredando de JFRAME

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,12 @@ import javax.swing.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
-    JFrame frame = new JFrame("Ventana");
-    frame.setContentPane(new Welcome().panel1);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.pack();
-    frame.setVisible(true);
-}
+        //JFrame frame = new JFrame("Ventana");
+        //frame.setContentPane(new Welcome().panel1);
+
+      //  frame.pack();
+        //  frame.setVisible(true);
+        new Welcome();
+    }
 }
