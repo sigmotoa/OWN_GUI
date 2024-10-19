2dcf39ccc2bed5dbc8236b524cc7c2ec6b2b59cd
sigmotoa <si.galvismotoa@gmail.com>
2024-10-19 07:40:31 -0500

    Se ajusto para llamar a la clase de crear archivos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import javax.sound.midi.Soundbank;
 import javax.swing.*;
+import java.io.IOException;
 
 public class Main {
 
@@ -8,6 +10,33 @@ public class Main {
 
       //  frame.pack();
         //  frame.setVisible(true);
-        new Welcome();
+        //new Welcome();
+
+        TextFileWriter writer = new TextFileWriter("archivo.txt");
+        try {
+            writer.openFile();
+            writer.writeToFile("Hola\n");
+            writer.writeToFile("Linea 3");
+            writer.readFile();
+
+            System.out.println("Todo correcto");
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                writer.closeFile();
+                //writer.openFile();
+                //writer.readFile();
+            }catch (IOException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        
+
     }
+
+
 }
